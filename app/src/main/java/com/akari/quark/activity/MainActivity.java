86a6ad1df78f2b9d95e8ea0dedb3e3512777f9ac
Akaza Akari <NonexistentAkarin@users.noreply.github.com>
2016-05-05 15:28:49 +0800

    Development:实现下拉刷新

--- app/src/main/java/com/akari/quark/activity/MainActivity.java
+++ app/src/main/java/com/akari/quark/activity/MainActivity.java
@@ -1,28 +1,53 @@
 package com.akari.quark.activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.akari.quark.R;
+import com.akari.quark.adapter.HomeAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,SwipeRefreshLayout.OnRefreshListener {
+
+    private Context context;
+    private Toolbar toolbar;
+    private SwipeRefreshLayout mSwipeLayout;
+    private ListView mListView;
+    private ArrayList<String> list = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        context = MainActivity.this;
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        init();
+    }
+
+
+    private void init(){
+        initView();
+        initEvent();
+    }
+
+    private void initView(){
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -42,6 +67,54 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void initEvent(){
+        mListView = (ListView) findViewById(R.id.listview);
+        mListView.setAdapter(new HomeAdapter<String>(this, android.R.layout.simple_list_item_1,
+                getData()));
+
+        mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        mSwipeLayout.setOnRefreshListener(this);
+        mSwipeLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light, android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+    }
+
+    private ArrayList<String> getData() {
+        list.add("Hello");
+        list.add("This is Akari");
+        list.add("An Android Developer");
+        list.add("Love Open Source");
+        list.add("My GitHub: NonexistentAkarin");
+        list.add("Hello");
+        list.add("This is Akari");
+        list.add("An Android Developer");
+        list.add("Love Open Source");
+        list.add("My GitHub: NonexistentAkarin");list.add("Hello");
+        list.add("This is Akari");
+        list.add("An Android Developer");
+        list.add("Love Open Source");
+        list.add("My GitHub: NonexistentAkarin");list.add("Hello");
+        list.add("This is Akari");
+        list.add("An Android Developer");
+        list.add("Love Open Source");
+        list.add("My GitHub: NonexistentAkarin");list.add("Hello");
+        list.add("This is Akari");
+        list.add("An Android Developer");
+        list.add("Love Open Source");
+        list.add("My GitHub: NonexistentAkarin");
+        return list;
+    }
+
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeLayout.setRefreshing(false);
+            }
+        }, 5000);
     }
 
     @Override
@@ -81,6 +154,7 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        toolbar.setTitle(item.getTitle());
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
