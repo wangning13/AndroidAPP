5095e50ccda8e6d26c327f1453b2606cb6ff6efa
wangning13 <1287983597@qq.com>
2016-05-05 14:43:13 +0800

    修改侧滑菜单样式，包括头像变圆，菜单修改等，传入所有图片

--- app/src/main/java/com/akari/quark/activity/MainActivity.java
+++ app/src/main/java/com/akari/quark/activity/MainActivity.java
@@ -82,17 +82,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_find) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_message) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_care_field) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_care_question) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_raise_question) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_answer_question) {
 
         }
 
