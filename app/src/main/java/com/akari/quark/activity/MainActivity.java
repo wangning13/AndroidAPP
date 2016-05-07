4c569e2bdf57e559fad499a81815988e30bb281e
Akari <307103041@qq.com>
2016-05-07 13:38:31 +0800

    Fix Bug: RefreshLayout修复

--- app/src/main/java/com/akari/quark/activity/MainActivity.java
+++ app/src/main/java/com/akari/quark/activity/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-    //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+//                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
                     Intent intent = new Intent(context, CreatQuestionActivity.class);
                     startActivity(intent);
                 }
