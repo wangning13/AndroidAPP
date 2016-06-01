94ddbe502a63aea9187c27110f8d567b3760906c
wangning13 <1287983597@qq.com>
2016-06-01 21:12:53 +0800

    添加启动页

--- app/src/main/java/com/akari/quark/ui/activity/SplashActivity.java
+++ app/src/main/java/com/akari/quark/ui/activity/SplashActivity.java
@@ -1,9 +1,32 @@
 package com.akari.quark.ui.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.akari.quark.R;
 
 /**
  * Created by motoon on 2016/6/1.
  */
 public class SplashActivity extends Activity {
+    private final int SPLASH_DISPLAY_LENGHT = 3000; // 延迟三秒
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+
+        }, SPLASH_DISPLAY_LENGHT);
+
+    }
 }
